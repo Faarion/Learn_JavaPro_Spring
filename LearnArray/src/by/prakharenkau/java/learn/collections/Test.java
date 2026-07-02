a8baffb6ab09e1393ff1fa96bcf8c6e0de255ebb
Yauheni Prakharenkau <oneraidzenya@gmail.com>
2026-07-02 18:02:35 +0300

    final example Java Pro 1.1 Java Collections Framework (Коллекции) - Динамический массив (Array List) - Введение
    й
    q
    clear

--- LearnArray/src/by/prakharenkau/java/learn/collections/Test.java
+++ LearnArray/src/by/prakharenkau/java/learn/collections/Test.java
@@ -1,10 +1,40 @@
 package by.prakharenkau.java.learn.collections;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		List<Integer> list = new ArrayList<>();
+		for (int i = 0; i < 10; i++) {
+			list.add(i);
+		}
+		
+//		System.out.println(arrayList.get(0));
+//		System.out.println(arrayList.get(99));
+//		
+//		System.out.println(arrayList.size());
+		
+//		for (int i = 0; i < arrayList.size(); i++) {
+//			System.out.print(arrayList.get(i) + " ");
+//		}
+//		
+//		System.out.println();
+//		
+//		for (Integer i: arrayList) {
+//			System.out.print(arrayList.get(i) + " ");
+//		}
+		
+//		list.remove(5);
+//		System.out.println(list.toString());
+		
+		// Много удалений из листа
+		// ArrayList переделываем в LinkedList
+		list = new LinkedList<>();
+		
+		
 	}
 
 }
