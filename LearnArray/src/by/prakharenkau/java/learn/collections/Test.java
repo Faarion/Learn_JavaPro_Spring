367db1755104a11df4b2310026d4d616f20ac503
Yauheni Prakharenkau <oneraidzenya@gmail.com>
2026-07-06 11:09:52 +0300

    Final example 1.6 Java Collection Framework (Коллекции) - LinkedHashMap и TreeMap

--- LearnArray/src/by/prakharenkau/java/learn/collections/Test.java
+++ LearnArray/src/by/prakharenkau/java/learn/collections/Test.java
@@ -1,27 +1,37 @@
 package by.prakharenkau.java.learn.collections;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class Test {
 
 	public static void main(String[] args) {
-		Map<Integer, String> map = new HashMap<Integer, String>();
+		Map<Integer, String> hashMap = new HashMap<Integer, String>();
+		Map<Integer, String> linkedHashMap = new LinkedHashMap<Integer, String>();
+		Map<Integer, String> treeMap = new TreeMap<Integer, String>();
 		
-		map.put(1, "One");
-		map.put(2, "Two");
-		map.put(3, "Three");
-		map.put(3, "Три");
+		System.out.println("Hash map:");
+		testMap(hashMap);
 		
-//		System.out.println(map);
-//		
-//		System.out.println(map.get(1));
-//		System.out.println(map.get(10));
+		System.out.println("\nLinkedHashMap");
+		testMap(linkedHashMap);
+		
+		System.out.println("\nTreeMap");
+		testMap(treeMap);
+	}
+	
+	public static void testMap(Map<Integer, String> map) {
+		map.put(39, "Bob");
+		map.put(12, "Mike");
+		map.put(78, "Tom");
+		map.put(0, "Tim");
+		map.put(1500, "Lewis");
+		map.put(7, "Bob");
 		
 		for (Map.Entry<Integer, String> entry : map.entrySet()) {
 			System.out.println(entry.getKey() + " : " + entry.getValue());
 		}
 	}
-	
-	
 }
