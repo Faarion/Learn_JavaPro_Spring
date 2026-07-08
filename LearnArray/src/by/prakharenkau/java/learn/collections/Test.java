752ce19edc4a4bf2f4de35c9e4d064cc1cb8e51e
Yauheni Prakharenkau <oneraidzenya@gmail.com>
2026-07-08 15:35:23 +0300

    Final Example 1.8 Java Collection Framewortk (Коллекции) - Методы hashcode() и equals()

--- LearnArray/src/by/prakharenkau/java/learn/collections/Test.java
+++ LearnArray/src/by/prakharenkau/java/learn/collections/Test.java
@@ -1,42 +1,61 @@
 package by.prakharenkau.java.learn.collections;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 
 public class Test {
 
 	public static void main(String[] args) {
-		Set<Integer> set1 = new HashSet<Integer>();
+		Map<Person, String> map = new HashMap<Person, String>();
+		Set<Person> set = new HashSet<Person>();
+		Person person1 = new Person(1, "Mark");
+		Person person2 = new Person(1, "Mark");
 		
-		set1.add(0);
-		set1.add(1);
-		set1.add(2);
-		set1.add(3);
-		set1.add(4);
-		set1.add(5);
+		map.put(person1, "strpers1");
+		map.put(person2, "strpers2");
 		
-		Set<Integer> set2 = new HashSet<Integer>();
+		set.add(person1);
+		set.add(person2);
 		
-		set2.add(2);
-		set2.add(3);
-		set2.add(4);
-		set2.add(5);
-		set2.add(6);
-		set2.add(7);
+		System.out.println(map);
+		System.out.println(set);
 		
-		//union - объединение множеств
-		Set<Integer> union = new HashSet<Integer>(set1);
-		union.addAll(set2);
-		System.out.println(union);
 		
-		//intersection - пересечение множеств
-		Set<Integer> intersection = new HashSet<Integer>(set1);
-		intersection.retainAll(set2);
-		System.out.println(intersection);
-		
-		//difference - разность множеств
-		Set<Integer> difference = new HashSet<Integer>(set1);
-		difference.removeAll(set2);
-		System.out.println(difference);
 	}
+}
+
+class Person{
+	private int id;
+	private String name;
+	public Person(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return "Person [id=" + id + ", name=" + name + "]";
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(Integer.valueOf(id), name);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		return id == other.id && Objects.equals(name, other.name);
+	}
+	
+	
+	
+	
 }
