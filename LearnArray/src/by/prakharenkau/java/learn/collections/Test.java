0686c136f37e4851301cc1cb952d38b1650214f5
Yauheni Prakharenkau <oneraidzenya@gmail.com>
2026-07-09 23:05:28 +0300

    Final Example 1.14 Java Collection Framework (Коллекции) - Стек (Stack)

--- LearnArray/src/by/prakharenkau/java/learn/collections/Test.java
+++ LearnArray/src/by/prakharenkau/java/learn/collections/Test.java
@@ -1,49 +1,25 @@
 package by.prakharenkau.java.learn.collections;
 
-import java.util.Queue;
-import java.util.concurrent.ArrayBlockingQueue;
+import java.util.Stack;
 
 public class Test {
 
 	public static void main(String[] args) {
-		Person person1 = new Person(1);
-		Person person2 = new Person(2);
-		Person person3 = new Person(3);
-		Person person4 = new Person(4);
+		Stack<Integer> stack = new Stack<Integer>();
 		
-//		Queue<Person> people = new LinkedList<Person>();
-		Queue<Person> people = new ArrayBlockingQueue<Person>(3);
+		stack.push(5);
+		stack.push(3);
+		stack.push(1);
 		
-		people.add(person3);
-		people.add(person2);
+//		System.out.println(stack.pop());
+//		System.out.println(stack.pop());
+//		System.out.println(stack.peek());
+//		System.out.println(stack.peek());
+//		System.out.println(stack.empty());
 		
-		System.out.println(people.offer(person4));
-		System.out.println(people.offer(person1));
-		
-		System.out.println("Remove:");
-		System.out.println(people.remove());
-		System.out.println("Peek");
-		System.out.println(people.peek());
-		
-		for (Person person : people) {
-			System.out.println(person);
+		while (!stack.empty()) {
+			System.out.println(stack.pop());
 		}
 		
 	}
-}
-
-class Person {
-	private int id;
-
-	public Person(int id) {
-		super();
-		this.id = id;
-	}
-
-	@Override
-	public String toString() {
-		return "Person [id=" + id + "]";
-	}
-	
-	
 }
