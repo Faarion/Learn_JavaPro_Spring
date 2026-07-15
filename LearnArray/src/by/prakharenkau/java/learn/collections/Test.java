418042bfb361ae0762e84f7e07247b707851b5c8
Yauheni Prakharenkau <oneraidzenya@gmail.com>
2026-07-16 01:43:09 +0300

    Final Example 2.23 - Паттерн producer - consumer (II часть)

--- LearnArray/src/by/prakharenkau/java/learn/collections/Test.java
+++ LearnArray/src/by/prakharenkau/java/learn/collections/Test.java
@@ -1,18 +1,20 @@
 package by.prakharenkau.java.learn.collections;
 
-import java.util.Scanner;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Random;
 
 public class Test {
-	
+			
 	public static void main(String[] args) throws InterruptedException {
-		WaitAndNotify wn = new WaitAndNotify();
+		ProduserConsumer ps = new ProduserConsumer();
 		
 		Thread thread1 = new Thread(new Runnable() {
 			
 			@Override
 			public void run() {
 				try {
-					wn.produce();
+					ps.produce();
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
@@ -24,7 +26,7 @@ public class Test {
 			@Override
 			public void run() {
 				try {
-					wn.consumer();
+					ps.consumer();
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
@@ -39,23 +41,38 @@ public class Test {
 	}
 }
 
-class WaitAndNotify {
+class ProduserConsumer {
+	private Queue<Integer> queue = new LinkedList<Integer>();
+	private final int LIMIT = 10;
+	private final Object lock = new Object();
+	
 	public void produce() throws InterruptedException {
-		synchronized (this) {
-			System.out.println("Producer thread started...");
-			wait();
-			System.out.println("Produser thread resumed...");
+		int value = 0;
+		while (true) {
+			synchronized (lock) {
+				while(queue.size() == LIMIT) {
+					lock.wait();
+				}
+				
+				queue.add(value++);
+				lock.notify();
+			}
 		}
 	}
 	
 	public void consumer() throws InterruptedException {
-		Thread.sleep(2000);
-		Scanner scanner = new Scanner(System.in);
-		
-		synchronized (this) {
-			System.out.println("Waiting for return key pressed");
-			scanner.nextLine();
-			notify();
+		while(true) {
+			synchronized (lock) {
+				while(queue.size() == 0) {
+					lock.wait();
+				}
+				
+				int value = queue.poll();
+				System.out.println(value);
+				System.out.println("Size: " + queue.size());
+				lock.notify();
+			}
+			Thread.sleep(1000);
 		}
 	}
 }
