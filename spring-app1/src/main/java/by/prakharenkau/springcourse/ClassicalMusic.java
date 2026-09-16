190ce168484dcd51df7a95ab9495e40bb02a0635
Yauheni Prakharenkau <oneraidzenya@gmail.com>
2026-09-16 21:16:10 +0300

    Spring course. Spring core. 2.9 Жизненный цикл бина (Bean Lifecycle). Init, Destroy и Factory методы.

--- spring-app1/src/main/java/by/prakharenkau/springcourse/ClassicalMusic.java
+++ spring-app1/src/main/java/by/prakharenkau/springcourse/ClassicalMusic.java
@@ -1,7 +1,21 @@
 package by.prakharenkau.springcourse;
 
 public class ClassicalMusic implements Music{
+	
+	private ClassicalMusic() {}
 
+	public void doMyInit() {
+		System.out.println("Doing my initialization");
+	}
+	
+	public void doMyDestroy() {
+		System.out.println("Doing my destruction");
+	}
+	
+	public static ClassicalMusic getClassicalMusic() {
+		return new ClassicalMusic();
+	}
+	
 	@Override
 	public String getSong() {
 		return "Hungarian Rhapsodi";
--- spring-app1/src/main/java/by/prakharenkau/springcourse/TestSpring.java
+++ spring-app1/src/main/java/by/prakharenkau/springcourse/TestSpring.java
@@ -8,6 +8,9 @@ public class TestSpring {
 //		Music music = context.getBean("musicBean", Music.class);
 //		MusicPlayer musicPlayer = new MusicPlayer(music);
 		
+		Music music = context.getBean("classicalMusicBean", ClassicalMusic.class);
+		System.out.println(music.getSong());
+		
 		MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
 		MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
 		//musicPlayer.playMusicList();
@@ -16,6 +19,7 @@ public class TestSpring {
 		firstMusicPlayer.setVolume(100);
 		System.out.println(firstMusicPlayer);
 		System.out.println(secondMusicPlayer);
+		
 		context.close(); 
 	}
 }
