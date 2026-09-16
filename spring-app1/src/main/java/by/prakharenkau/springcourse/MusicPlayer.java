d7b5ea51f5fe56c534d4d022ea581167f772d6b7
Yauheni Prakharenkau <oneraidzenya@gmail.com>
2026-09-16 16:43:17 +0300

    Spring course. Spring Core. 2.7 Внедрение зависимостей. Dependecy Injection (DI). Часть 2.

--- spring-app1/src/main/java/by/prakharenkau/springcourse/MusicPlayer.java
+++ spring-app1/src/main/java/by/prakharenkau/springcourse/MusicPlayer.java
@@ -1,13 +1,44 @@
 package by.prakharenkau.springcourse;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MusicPlayer {
-	private Music music;
+	private List<Music> musicList = new ArrayList<Music>();
 	
-	public MusicPlayer(Music music) {
-		this.music = music;
+	private String name;
+	private int volume;
+	
+	
+	public MusicPlayer() {}
+	
+	public void playMusicList() {
+		for (Music music : musicList) {
+			System.out.println("Playing: " + music.getSong());
+		}
 	}
-	
-	public void playMusic() {
-		System.out.println("Playing " + music.getSong());
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getVolume() {
+		return volume;
+	}
+
+	public void setVolume(int volume) {
+		this.volume = volume;
+	}
+
+	public List<Music> getMusicList() {
+		return musicList;
+	}
+
+	public void setMusicList(List<Music> musicList) {
+		this.musicList = musicList;
 	}
 }
